bc058fcd8b5745bbc31fd907f724561e1817b075
Brian Warner <brianwarner@Brians-MacBook-Air.local>
2018-04-05 11:09:53 -0400

    BW/10 citation commit.

--- src/main/java/com/bwarner/enums/Methods.java
+++ src/main/java/com/bwarner/enums/Methods.java
@@ -1,5 +1,7 @@
 package com.bwarner.enums;
 
+/** Borrowed this from elsewhere, removed types I won't be supporting within the context of this exercise **/
+
 public enum Methods {
     GET("GET"), //
     HEAD("HEAD"), //
